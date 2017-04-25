f80fdeec86e33b878ccb82a10cd3162ed865099c
Graziella-Husson <graziella.husson1@gmail.com>
2017-04-25 19:06:57 +0200

    a

--- webPLM-master/public/app/commentary/commentary.controller.js
+++ webPLM-master/public/app/commentary/commentary.controller.js
@@ -10,21 +10,17 @@
   function Commentary($http, $scope, $state, $sce, langs, connection, listenersHandler, navigation, gettextCatalog) {
     var commentary = this;
 
-    commentary.lessons = [];
+    /*commentary.lessons = [];
     commentary.currentLesson = null;
     commentary.currentExerciseID = '';
-
     commentary.getLessons = getLessons;
     commentary.setLessons = setLessons;
     commentary.setCurrentLesson = setCurrentLesson;
     commentary.goToLesson = goToLesson;
-
     navigation.setCurrentPageTitle(gettextCatalog.getString('Commentary'));
-
     var offHandleMessage = listenersHandler.register('onmessage', handleMessage);
-
     getLessons();
-
+ */
     function handleMessage(data) {
       var cmd = data.cmd;
       var args = data.args;
@@ -37,30 +33,27 @@
         break;
       }
     }
-
+/*
     function getLessons() {
       connection.sendMessage('getLessons', null);
     }
-
     function setLessons(lessons) {
-      home.lessons = lessons.map(function (lesson) {
+      controller.lessons = lessons.map(function (lesson) {
         lesson.description = $sce.trustAsHtml(lesson.description);
         return lesson;
       });
-      home.currentLesson = null;
+      controller.currentLesson = null;
       console.log('updated commentary.lessons: ', commentary.lessons);
     }
-
     function setCurrentLesson(lesson) {
-      home.currentLesson = lesson;
+      controller.currentLesson = lesson;
     }
-
     function goToLesson() {
       $state.go('exercise', {
         'lessonID': commentary.currentLesson.id
       });
     }
-
+*/
     $scope.$on('$destroy', function () {
       offHandleMessage();
     });
